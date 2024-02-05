b916f81e787f877eb728761a678928d04c99fa9b
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-02-05 12:13:33 +0530

    Pre-order Traversal in Binary Tree.cpp

--- Pre-order Traversal in Binary Tree.cpp
+++ Pre-order Traversal in Binary Tree.cpp
@@ -67,3 +67,28 @@ public:
 
 Code: (Iterative):
 -------------------
+vector < int > preOrder;
+  if (curr == NULL)
+    return preOrder;
+
+  stack < node * > s;
+  s.push(curr);
+
+  while (!s.empty()) {
+    node * topNode = s.top();
+    preOrder.push_back(topNode -> data);
+    s.pop();
+    if (topNode -> right != NULL)
+      s.push(topNode -> right);
+    if (topNode -> left != NULL)
+      s.push(topNode -> left);
+  }
+  return preOrder;
+
+}
+
+
+
+
+
+	
