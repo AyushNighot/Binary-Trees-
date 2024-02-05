6e63709850d98e97e6faf1c3892fbbaefbca0a8c
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-02-05 12:24:28 +0530

    Update InOrder Traversal In Binary tree.cpp

--- InOrder Traversal In Binary tree.cpp
+++ InOrder Traversal In Binary tree.cpp
@@ -68,3 +68,18 @@ public:
 
 Code - Iterative:
 ------------------
+  vector < int > inOrder;
+  stack < node * > s;
+  while (true) {
+    if (curr != NULL) {
+      s.push(curr);
+      curr = curr -> left;
+    } else {
+      if (s.empty()) break;
+      curr = s.top();
+      inOrder.push_back(curr -> data);
+      s.pop();
+      curr = curr -> right;
+    }
+  }
+  return inOrder;
